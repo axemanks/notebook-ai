c496ef9e7fb93ce08209d09b9ca7923ca424a350
KeithScheldt <keithscheldt@gmail.com>
2023-10-06 11:29:24 -0400

    updated title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import Provider from '@/components/Provider';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Notion-AI Clone',
+  title: 'Notebook-AI',
   description: 'Built by Keith Scheldt',
 };
 
